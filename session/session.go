dfd7f3fdc06e82eb13ff3452c65ed9bec7090923
Carsten Leue <carsten.leue@de.ibm.com>
2026-08-25 15:38:46 +0200

    fix: almost completed Next call
    Signed-off-by: Carsten Leue <carsten.leue@de.ibm.com>

--- session/session.go
+++ session/session.go
@@ -2,16 +2,27 @@ package session
 
 import (
 	oai "github.com/Carsten-Leue/fp-go-harness/openai"
+	"github.com/Carsten-Leue/fp-go-harness/tools"
+	A "github.com/IBM/fp-go/v2/array"
 	"github.com/IBM/fp-go/v2/effect"
+	"github.com/IBM/fp-go/v2/endomorphism"
 	F "github.com/IBM/fp-go/v2/function"
 	I "github.com/IBM/fp-go/v2/identity"
 	N "github.com/IBM/fp-go/v2/number"
 	L "github.com/IBM/fp-go/v2/optics/lens"
+	LP "github.com/IBM/fp-go/v2/optics/lens/prism"
+	OL "github.com/IBM/fp-go/v2/optics/optional/lens"
+	P "github.com/IBM/fp-go/v2/optics/prism"
+	"github.com/IBM/fp-go/v2/option"
 	"github.com/IBM/fp-go/v2/pair"
 	"github.com/IBM/fp-go/v2/reader"
+	"github.com/IBM/fp-go/v2/readeroption"
+	S "github.com/IBM/fp-go/v2/string"
 	"github.com/IBM/fp-go/v2/tailrec"
 	"github.com/openai/openai-go/v3"
 )
+
+const finishReasonToolCalls = "tool_calls"
 
 type HistoryEntry = pair.Pair[openai.ChatCompletionNewParams, *openai.ChatCompletion]
 
@@ -38,17 +49,35 @@ func MakeSession(req openai.ChatCompletionNewParams) Session {
 	return Session{current: req}
 }
 
+// TODO move to library
+func headPrism[T any]() Prism[[]T, T] {
+	return P.MakePrism(A.Head[T], A.Of)
+}
+
+func isToolCallFinishReason() Predicate[string] {
+	return S.Equals(finishReasonToolCalls)
+}
+
 func Next() effect.Kleisli[SessionDeps, Session, NextStep] {
 
 	currentLens := MakeSessioncurrentLens()
+	historyLens := MakeSessionhistoryLens()
 	iterLens := MakeSessioniterationsLens()
 	usageLens := MakeSessionusageLens()
 	usageFromCompletionLens := oai.MakeChatCompletionUsageRefLens()
+	choicesLens := oai.MakeChatCompletionChoicesRefLens()
+	finishReasonLens := oai.MakeChatCompletionChoiceFinishReasonLens()
 
-	// handleToolCalls := F.Flow2(
-	// 	tools.HandleToolCalls(),
-	// 	effect.Local[Endomorphism[openai.ChatCompletionNewParams]](SessionDeps.GetToolCaller),
-	// )
+	firstFinishReason := F.Pipe2(
+		choicesLens,
+		LP.Compose[*openai.ChatCompletion](headPrism[openai.ChatCompletionChoice]()),
+		OL.Compose[*openai.ChatCompletion](finishReasonLens),
+	)
+
+	handleToolCalls := F.Flow2(
+		tools.HandleToolCalls(),
+		effect.Local[Endomorphism[openai.ChatCompletionNewParams]](SessionDeps.GetToolCaller),
+	)
 	chatCompletion := F.Flow2(
 		oai.ChatCompletion(),
 		effect.Local[*openai.ChatCompletion](oai.AsChatCompletionDeps[SessionDeps]),
@@ -69,21 +98,85 @@ func Next() effect.Kleisli[SessionDeps, Session, NextStep] {
 		)),
 	)
 
-	dispatchToolCall := func(r FinalResult) Effect[SessionDeps, NextStep] {
-		// TODO dispatch dependening on state
-		return effect.Of[SessionDeps](tailrec.Land[Session](r))
+	addToHistory := func(r FinalResult) FinalResult {
+
+		x := F.Pipe1(
+			pair.Tail[Session, *openai.ChatCompletion],
+			reader.ApS(
+				pair.FromHead[*openai.ChatCompletion, openai.ChatCompletionNewParams],
+				F.Flow3(
+					reader.Ask[FinalResult](),
+					pair.Head[Session, *openai.ChatCompletion],
+					currentLens.Get,
+				),
+			),
+			// reader.ApS(
+			// 	func(History) func(),
+			// 	F.Flow3(
+			// 		reader.Ask[FinalResult](),
+			// 		pair.Head[Session, *openai.ChatCompletion],
+			// 		historyLens.Get,
+			// 	),
+			// ),
+		)
 	}
+
+	toFinalResult := F.Flow2(
+		reader.Of[Session, Effect[SessionDeps, *openai.ChatCompletion]],
+		reader.ApS(F.Flow2(
+			pair.FromHead[*openai.ChatCompletion, Session],
+			effect.Map[SessionDeps],
+		), reader.Ask[Session]()),
+	)
+
+	bounceToolCall := F.Pipe4(
+		addToHistory,
+		reader.Map[FinalResult](F.Flow2(
+			pair.Tail[Session, *openai.ChatCompletion],
+			handleToolCalls,
+		)),
+		reader.ApS(
+			F.Flow2(
+				endomorphism.Read[openai.ChatCompletionNewParams],
+				effect.Map[SessionDeps],
+			), F.Flow3(
+				reader.Ask[FinalResult](),
+				pair.Head[Session, *openai.ChatCompletion],
+				currentLens.Get,
+			),
+		),
+		reader.Chain(F.Flow2(
+			reader.Of[FinalResult],
+			reader.ApS(
+				F.Flow2(
+					reader.Sequence(currentLens.Set),
+					effect.Map[SessionDeps],
+				), F.Flow2(
+					reader.Ask[FinalResult](),
+					pair.Head[Session, *openai.ChatCompletion],
+				),
+			),
+		)),
+		reader.Map[FinalResult](effect.Map[SessionDeps](tailrec.Bounce[FinalResult, Session])),
+	)
+
+	landFinalResult := F.Flow2(
+		tailrec.Land[Session, FinalResult],
+		effect.Of[SessionDeps],
+	)
+
+	dispatch := F.Pipe4(
+		pair.Tail[Session, *openai.ChatCompletion],
+		reader.Map[FinalResult](firstFinishReason.GetOption),
+		readeroption.ChainOptionK[FinalResult](option.FromPredicate(isToolCallFinishReason())),
+		readeroption.ChainReaderK(reader.Of[string](bounceToolCall)),
+		readeroption.GetOrElse(landFinalResult),
+	)
 
 	return F.Pipe3(
 		currentLens.Get,
 		reader.Map[Session](chatCompletion),
-		reader.Chain(F.Flow2(
-			reader.Of[Session, Effect[SessionDeps, *openai.ChatCompletion]],
-			reader.ApS(F.Flow2(
-				pair.FromHead[*openai.ChatCompletion, Session],
-				effect.Map[SessionDeps],
-			), reader.Ask[Session]()),
-		)),
+		reader.Chain(toFinalResult),
 		reader.Map[Session](F.Flow2(
 			effect.Map[SessionDeps](F.Flow2(
 				pair.MapHead[*openai.ChatCompletion](incIterations),
@@ -96,7 +189,7 @@ func Next() effect.Kleisli[SessionDeps, Session, NextStep] {
 					),
 				),
 			)),
-			effect.Chain(dispatchToolCall),
+			effect.Chain(dispatch),
 		)),
 	)
 }
--- session/types.go
+++ session/types.go
@@ -4,7 +4,9 @@ import (
 	"github.com/Carsten-Leue/fp-go-harness/tools"
 	"github.com/IBM/fp-go/v2/effect"
 	"github.com/IBM/fp-go/v2/endomorphism"
+	"github.com/IBM/fp-go/v2/optics/prism"
 	"github.com/IBM/fp-go/v2/pair"
+	"github.com/IBM/fp-go/v2/predicate"
 	"github.com/IBM/fp-go/v2/reader"
 	"github.com/IBM/fp-go/v2/tailrec"
 )
@@ -17,4 +19,6 @@ type (
 	Endomorphism[A any]  = endomorphism.Endomorphism[A]
 	Reader[R, A any]     = reader.Reader[R, A]
 	Pair[L, R any]       = pair.Pair[L, R]
+	Prism[S, A any]      = prism.Prism[S, A]
+	Predicate[A any]     = predicate.Predicate[A]
 )
